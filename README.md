c10aafc63043af6f9d225ba454e8bda80d6bbd2a
gabriel <gabriel.job@aprix.com.br>
2021-01-23 15:17:31 -0300

    setup_venv.sh fix

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ Stacked Autoencoder Q-Network algorithm for Spark message queues management.
 
 -Installing dependencies:
 	~~~~
+	sudo apt update
+	sudo apt install python3-pip
+	sudo apt install python3-venv
 	./setup_venv.sh
 	~~~~
 
--- setup_venv.sh
+++ setup_venv.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-VENV_DIR = 'venv'
+VENV_DIR=./venv
 
-if  [[ ! -d "$VENV_DIR" ]]
+if  [[ ! -d $VENV_DIR ]]
 then
-    mkdir "$VENV_DIR"
+    #mkdir $VENV_DIR
+    python3 -m venv $VENV_DIR
 fi
 
-source ./venv/bin/activate
+source $VENV_DIR/bin/activate
 pip install -r requirements.txt
 deactivate
